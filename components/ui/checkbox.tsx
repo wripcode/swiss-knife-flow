8051d8acb5a8b9ee26200e0fcb725a4afde73373
wripcode <gaurang.hv@gmail.com>
2026-03-28 13:50:48 +0530

    refactor: ui/checkbox

--- components/ui/checkbox.tsx
+++ components/ui/checkbox.tsx
@@ -14,7 +14,10 @@ function Checkbox({
     <CheckboxPrimitive.Root
       data-slot="checkbox"
       className={cn(
-        "peer border-border dark:bg-input/30 data-[state=checked]:bg-primary data-[state=checked]:text-primary-foreground dark:data-[state=checked]:bg-primary data-[state=checked]:border-primary focus-visible:border-ring focus-visible:ring-ring/50 aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive size-4 shrink-0 rounded-[4px] border shadow-xs transition-shadow outline-none focus-visible:ring-[3px] disabled:cursor-not-allowed disabled:opacity-50",
+        "peer size-4 shrink-0 rounded border border-border bg-(--background-input) shadow-(--input-inner-shadow) outline-none transition-shadow disabled:cursor-not-allowed disabled:opacity-50",
+        "data-[state=checked]:bg-primary data-[state=checked]:border-primary data-[state=checked]:text-primary-foreground",
+        "focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[1px]",
+        "aria-invalid:ring-destructive/20 aria-invalid:border-destructive dark:aria-invalid:ring-destructive/40",
         className
       )}
       {...props}
